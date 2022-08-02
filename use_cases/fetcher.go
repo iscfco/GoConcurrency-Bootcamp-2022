654c4618de39d0e25100df0a2293bba3147a52a5
Francisco Puc Poot <isc.fco@gmail.com>
2022-08-02 00:07:31 -0500

    added context cancellation for api/provide endpoint

--- use_cases/fetcher.go
+++ use_cases/fetcher.go
@@ -1,9 +1,11 @@
 package use_cases
 
 import (
+	"context"
 	"log"
 	"strings"
 	"sync"
+	"time"
 
 	"GoConcurrency-Bootcamp-2022/models"
 )
@@ -25,8 +27,8 @@ func NewFetcher(api api, storage writer) Fetcher {
 	return Fetcher{api, storage}
 }
 
-func (f Fetcher) Fetch(from, to int) error {
-	pokeChannel := f.generatePokeStream(from, to)
+func (f Fetcher) Fetch(ctx context.Context, from, to int) error {
+	pokeChannel := f.generatePokeStream(ctx, from, to)
 
 	var pokemons []models.Pokemon
 	for pokemon := range pokeChannel {
@@ -42,24 +44,46 @@ func (f Fetcher) Fetch(from, to int) error {
 	return f.storage.Write(pokemons)
 }
 
-func (f Fetcher) generatePokeStream(from, to int) chan models.Pokemon {
+func (f Fetcher) generatePokeStream(ctx context.Context, from, to int) chan models.Pokemon {
+	ctx, cancelFn := context.WithCancel(ctx)
 	ch := make(chan models.Pokemon)
 	wg := sync.WaitGroup{}
 
-	for id := from; id <= to; id++ {
-		wg.Add(1)
+	wg.Add(1)
+	go func(ctx context.Context, cancelFn context.CancelFunc) {
+		defer wg.Done()
 
-		go func(id int) {
-			defer wg.Done()
-
-			pokemon, err := f.api.FetchPokemon(id)
-			if err != nil {
-				log.Printf("cannot get id: %d, due to err: %v", id, err)
+		for id := from; id <= to; id++ {
+			if ctx.Err() != nil {
+				log.Printf("breaking 'For' statement from: %d in order to avoid performing unnecessary goroutines", id)
+				return
 			}
 
-			ch <- pokemon
-		}(id)
-	}
+			if id > 1000 {
+				time.Sleep(time.Duration(100) * time.Millisecond)
+			}
+
+			wg.Add(1)
+			go func(ctx context.Context, cancelFn context.CancelFunc, id int) {
+				defer wg.Done()
+				time.Sleep(time.Duration(100+id) * time.Millisecond)
+
+				if ctx.Err() != nil {
+					log.Printf("breaking goroutine (#%d) process due to invalid context: %v\n", id, ctx.Err())
+					return
+				}
+
+				pokemon, err := f.api.FetchPokemon(id)
+				if err != nil {
+					log.Printf("cannot get id: %d, due to err: %v\n", id, err)
+					cancelFn()
+				}
+				ch <- pokemon
+
+			}(ctx, cancelFn, id)
+
+		}
+	}(ctx, cancelFn)
 
 	go func() {
 		wg.Wait()
